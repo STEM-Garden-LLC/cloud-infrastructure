e9bc4ab4883da744a62dd181376c361ede3e4c75
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-11 21:54:27 -0600

    Create backend.tf

---
+++ workspaces/sandbox/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+    cloud {
+        hostname = "app.terraform.io"
+        organization = "stem-garden-llc"
+
+        workspaces {
+            name = "sandbox"
+            tags = ["source:cli"]
+        }
+    }
+}
