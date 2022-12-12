f04be31b7f2eb6b195c6c58d904bdad89693edcc
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-11 22:37:51 -0600

    Add config files and specify aws profile

--- workspaces/sandbox/providers.tf
+++ workspaces/sandbox/providers.tf
@@ -1,3 +1,7 @@
 provider "aws" {
   region = var.region
+
+  shared_config_files      = ["/Users/tf_user/.aws/config"]
+  shared_credentials_files = ["/Users/tf_user/.aws/credentials"]
+  profile                  = "sgllc-admin"
 }
