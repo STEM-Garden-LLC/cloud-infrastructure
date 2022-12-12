eab9f9e632fed529280dc86c7eda90e91629be8e
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-11 23:01:05 -0600

    Delete profile, paths to shared config and credentials files

--- workspaces/sandbox/providers.tf
+++ workspaces/sandbox/providers.tf
@@ -1,7 +1,4 @@
 provider "aws" {
   region = var.region
 
-  shared_config_files      = ["/Users/tf_user/.aws/config"]
-  shared_credentials_files = ["/Users/tf_user/.aws/credentials"]
-#   profile                  = "stem-garden-llc"
 }
