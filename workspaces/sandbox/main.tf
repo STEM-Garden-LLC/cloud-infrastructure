b447bf87dfc60d8c443f07265e8bb6e71326d4c1
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-15 16:27:48 -0600

    Add EC2 instance and vars
    Just to test it out and familiarize myself with the resource.

--- workspaces/sandbox/main.tf
+++ workspaces/sandbox/main.tf
@@ -19,3 +19,27 @@ resource "aws_lambda_function" "access_key_generator" {
   timeout          = 30
   tags             = local.tags
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "test_ec2_instance" {
+  ami = var.ec2_ami
+  instance_type =  var.instance_type
+  
+  availability_zone = var.availability_zone
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  associate_public_ip_address = false
+  
+  # Cannot be used in combination with aws_ebs_volume and aws_volume_attachment resources on a given instance.
+  ebs_block_device = {
+    volume_type = standard
+    volume_size = 20 # GiB
+  }
+
+  host_resource_group_arn = var.host_resource_group_arn
+
+  # placement_group = null # How is placement group different from resource group?
+  # tenancy = # If the instance is in a VPC / on dedicated hardware. Enum: default, dedicated, host
+
+}
