723e91b44609e738562592789ea7f391ff50a4b7
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-19 16:59:41 -0500

    Uncomment VPC blank resource

--- workspaces/network/main.tf
+++ workspaces/network/main.tf
@@ -3,6 +3,6 @@
 #   assume_role_policy = data.aws_iam_policy_document.assume.json
 # }
 
-# resource "aws_vpc" "vpc_ohio" {
-#   # (resource arguments)
-# }
+resource "aws_vpc" "vpc_ohio" {
+  # (resource arguments)
+}
