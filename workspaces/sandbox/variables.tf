83093306e77290f2a076d4567ca01f6f17e848ad
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-20 12:43:06 -0500

    Add Workspace name as input var

--- workspaces/sandbox/variables.tf
+++ workspaces/sandbox/variables.tf
@@ -1,3 +1,8 @@
+variable "workspace" {
+  description = "The Terraform Cloud Workspace. Corresponds to the name of this directory."
+  type = string
+}
+
 variable "region" {
   description = "The AWS region the workspace corresponds to."
   type = string
