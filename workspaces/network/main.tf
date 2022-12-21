1151bb251585992d4a73b1a391178c5bb5228bb8
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-21 13:26:02 -0500

    Rename VPC and add tags

--- workspaces/network/main.tf
+++ workspaces/network/main.tf
@@ -3,11 +3,23 @@
 #   assume_role_policy = data.aws_iam_policy_document.assume.json
 # }
 
-resource "aws_vpc" "vpc_ohio" {
+resource "aws_vpc" "vpc" {
+  #   name = format("sgllc-vpc-%s", var.region)
+  # Region is not an argument to VPC, it is inherited from the AWS Provider
+
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default" # Only other option is "dedicated"
 
-  enable_dns_support = true     # default is true
+  enable_dns_support   = true # default is true
+  enable_dns_hostnames = true # default is __?
+
+  tags = merge(
+    local.tags,
+    {
+      Name = format("sgllc-vpc-%s", var.region)
+    }
+  )
+}
 
   tags = local.tags
 }
