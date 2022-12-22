fc8d2e898c570dc19f0cd132663b9233c288160b
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-22 11:36:32 -0500

    Fix parent_id reference

--- workspaces/organization/accounts_sandbox.tf
+++ workspaces/organization/accounts_sandbox.tf
@@ -3,7 +3,7 @@
 resource "aws_organizations_account" "sgllc_sandbox" {
   name      = "SGLLC Sandbox"
   email     = "aws+sandbox@stemgarden.org"
-  parent_id = aws_organizations_organizational_unit.org_sandbox
+  parent_id = aws_organizations_organizational_unit.org_sandbox.id
 
   role_name = "Admin"
 }
