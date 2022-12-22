e0f026554b55322ce117296fc5189e9b53169f68
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-21 23:36:22 -0500

    Update accounts.tf

--- workspaces/organization/accounts.tf
+++ workspaces/organization/accounts.tf
@@ -47,3 +47,28 @@ resource "aws_organizations_account" "mastery_math_prod" {
   name  = "Mastery Math Prod"
   email = "mastery_math+prod@stemgarden.org"
 }
+
+
+
+# ACCOUNTS
+
+
+# SEE https://tbekas.dev/posts/setting-up-an-aws-organization-from-scratch-with-terraform
+
+# resource "aws_organizations_account" "users" {
+#   name      = "acme-corp-users"
+#   email     = "admin+users@acmecorp.com"
+#   role_name = "Admin"
+# }
+
+# resource "aws_organizations_account" "staging" {
+#   name      = "acme-corp-staging"
+#   email     = "admin+staging@acmecorp.com"
+#   role_name = "Admin"
+# }
+
+# resource "aws_organizations_account" "production" {
+#   name      = "acme-corp-production"
+#   email     = "admin+production@acmecorp.com"
+#   role_name = "Admin"
+# }
