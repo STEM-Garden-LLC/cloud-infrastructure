b2da17f67ab69f99a11968cef17a5c5e0bfa57bd
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-22 11:30:18 -0500

    Extract management accounts to own file

--- workspaces/organization/accounts.tf
+++ workspaces/organization/accounts.tf
@@ -1,14 +1,5 @@
 # ACCOUNTS
 
-## MANAGEMENT ACCOUNT
-
-resource "aws_organizations_account" "stem_garden_llc" {
-  name  = "STEM Garden LLC"
-  email = "nigel@stemgarden.org"
-  #   role_name = "Admin"
-  # management_account
-}
-
 
 ## MASTERY MATH 
 
@@ -49,26 +40,3 @@ resource "aws_organizations_account" "mastery_math_prod" {
 }
 
 
-
-# ACCOUNTS
-
-
-# SEE https://tbekas.dev/posts/setting-up-an-aws-organization-from-scratch-with-terraform
-
-# resource "aws_organizations_account" "users" {
-#   name      = "acme-corp-users"
-#   email     = "admin+users@acmecorp.com"
-#   role_name = "Admin"
-# }
-
-# resource "aws_organizations_account" "staging" {
-#   name      = "acme-corp-staging"
-#   email     = "admin+staging@acmecorp.com"
-#   role_name = "Admin"
-# }
-
-# resource "aws_organizations_account" "production" {
-#   name      = "acme-corp-production"
-#   email     = "admin+production@acmecorp.com"
-#   role_name = "Admin"
-# }
