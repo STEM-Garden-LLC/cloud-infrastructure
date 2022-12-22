8ee9141cd23eda1f7ea765c71876a1c365f4c59d
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-22 11:31:28 -0500

    Extract Mastery Math Accounts

---
+++ workspaces/organization/accounts_mm.tf
@@ -0,0 +1,49 @@
+# Accounts beloning to the MASTERY MATH Organizational Unit
+
+### Dev
+
+resource "aws_organizations_account" "mm_dev" {
+  name      = "Mastery Math Dev"
+  email     = "mm-dev@stemgarden.org"
+  parent_id = aws_organizations_organizational_unit.mastery_math.id
+}
+
+resource "aws_organizations_account" "mastery_math_dev" {
+  name      = "Mastery Math Dev"
+  email     = "masterymath+dev@stemgarden.org"
+  parent_id = aws_organizations_organizational_unit.mastery_math.id
+}
+
+### Staging
+
+resource "aws_organizations_account" "mm_staging" {
+  name      = "Mastery Math Staging"
+  email     = "mm-staging@stemgarden.org"
+  parent_id = aws_organizations_organizational_unit.mastery_math.id
+}
+
+resource "aws_organizations_account" "mm_staging_real" {
+  name      = "Mastery Math Staging"
+  email     = "mm-staging@stemgarden.org"
+  parent_id = aws_organizations_organizational_unit.mastery_math.id
+}
+
+resource "aws_organizations_account" "mastery_math_stage" {
+  name      = "Mastery Math Stage"
+  email     = "mastery_math+stage@stemgarden.org"
+  parent_id = aws_organizations_organizational_unit.mastery_math.id
+}
+
+### Prod 
+
+resource "aws_organizations_account" "mm_prod" {
+  name      = "Mastery Math Prod"
+  email     = "masterymath@stemgarden.org"
+  parent_id = aws_organizations_organizational_unit.mastery_math.id
+}
+
+resource "aws_organizations_account" "mastery_math_prod" {
+  name      = "Mastery Math Prod"
+  email     = "mastery_math+prod@stemgarden.org"
+  parent_id = aws_organizations_organizational_unit.mastery_math.id
+}
