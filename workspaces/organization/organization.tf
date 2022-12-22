0dcb1b06516c6de92fafc2c0717f3c2683ff913a
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-21 22:10:45 -0500

    Prepare to import management account

--- workspaces/organization/organization.tf
+++ workspaces/organization/organization.tf
@@ -1,6 +1,19 @@
-# resource "aws_organizations_organization" "sgllc_org" {
+resource "aws_organizations_organization" "sgllc_org" {
 
-#     feature_set = "ALL"
+    feature_set = "ALL"
+
+}
+
+# ACCOUNTS
+
+resource "aws_organizations_account" "stem_garden_llc" {
+
+#   name      = "STEM Garden LLC"
+#   email     = "nigel@stemgarden.org"
+#   role_name = "Admin"
+
+# management_account
+}
 
 # }
 
