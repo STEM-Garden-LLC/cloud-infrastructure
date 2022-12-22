cfb955be820b5abf3a6539154fbb115321550a8c
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-22 12:13:21 -0500

    Comment out accounts and leave explanatory note

--- workspaces/organization/accounts_sghp.tf
+++ workspaces/organization/accounts_sghp.tf
@@ -1,13 +1,14 @@
 # Accounts for managing the various environments of the STEM Garden Homepage
 
 ### Dev
-resource "aws_organizations_account" "sghp_dev" {
-  name      = "STEM Garden Homepage Dev"
-  email     = "aws+dev@stemgarden.org"
-  parent_id = aws_organizations_organizational_unit.stem_garden_homepage.id
-  role_name = "Admin"
 
-}
+# Uncomment and Apply once there is room in the Quota
+# resource "aws_organizations_account" "sghp_dev" {
+#   name      = "STEM Garden Homepage Dev"
+#   email     = "aws+dev@stemgarden.org"
+#   parent_id = aws_organizations_organizational_unit.stem_garden_homepage.id
+#   role_name = "Admin"
+# }
 
 # DO NOT IMPORT. SUSPENDED. CLOSE PENDING 90 DAY WIAT
 # resource "aws_organizations_account" "mastery_math_dev" {
@@ -18,13 +19,13 @@ resource "aws_organizations_account" "sghp_dev" {
 
 ### Stage
 
-resource "aws_organizations_account" "sghp_stage" {
-  name      = "STEM Garden Homepage Stage"
-  email     = "aws+stage@stemgarden.org"
-  parent_id = aws_organizations_organizational_unit.stem_garden_homepage.id
-  role_name = "Admin"
-
-}
+# # Uncomment and Apply once there is room in the Quota
+# resource "aws_organizations_account" "sghp_stage" {
+#   name      = "STEM Garden Homepage Stage"
+#   email     = "aws+stage@stemgarden.org"
+#   parent_id = aws_organizations_organizational_unit.stem_garden_homepage.id
+#   role_name = "Admin"
+# }
 
 # DO NOT IMPORT. PLAN TO SUSPEND & CLOSE PENDING 90 DAY WIAT & QUOTA RESET
 # resource "aws_organizations_account" "sg-staging" {
@@ -35,13 +36,13 @@ resource "aws_organizations_account" "sghp_stage" {
 
 ### Prod 
 
-resource "aws_organizations_account" "sghp_prod" {
-  name      = "STEM Garden Homepage Prod"
-  email     = "aws+prod@stemgarden.org"
-  parent_id = aws_organizations_organizational_unit.stem_garden_homepage.id
-  role_name = "Admin"
-
-}
+# Uncomment and Apply once there is room in the Quota
+# resource "aws_organizations_account" "sghp_prod" {
+#   name      = "STEM Garden Homepage Prod"
+#   email     = "aws+prod@stemgarden.org"
+#   parent_id = aws_organizations_organizational_unit.stem_garden_homepage.id
+#   role_name = "Admin"
+# }
 
 # DO NOT IMPORT. SUSPENDED. CLOSE PENDING 90 DAY WIAT
 # resource "aws_organizations_account" "sg_prod" {
