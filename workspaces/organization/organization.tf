2c53c87c6c16153e89c7c0a692969882d4328910
Nigel Wilson <nigel.wilson@airtonomy.ai>
2022-12-28 11:26:19 -0500

    Move mm abnd sghp OUs into new Projects OU

--- workspaces/organization/organization.tf
+++ workspaces/organization/organization.tf
@@ -11,19 +11,27 @@ resource "aws_organizations_organization" "sgllc_org" {
 
 # ORGANIZATIONAL UNITS -- LAYER 1
 
+resource "aws_organizations_organizational_unit" "projects" {
+  name      = "Projects"
+  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
+}
+
 resource "aws_organizations_organizational_unit" "org_sandbox" {
   name      = "Org Sandbox"
   parent_id = aws_organizations_organization.sgllc_org.roots[0].id
 }
 
+# ORGANIZATIONAL UNITS -- LAYER 2
+
+
 resource "aws_organizations_organizational_unit" "mastery_math" {
   name      = "Mastery Math"
-  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
+  parent_id = aws_organizations_organizational_unit.projects.id
 }
 
 resource "aws_organizations_organizational_unit" "stem_garden_homepage" {
   name      = "STEM Garden Homepage"
-  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
+  parent_id = aws_organizations_organizational_unit.projects.id
 }
 
 
