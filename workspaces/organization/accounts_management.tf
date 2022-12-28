a507ed5af07783c98d4274e4c78a3726d1c279f3
Nigel Wilson <nigel@stemgarden.org>
2022-12-28 10:00:58 -0600

    Add user management account (#18)
    * Add user management account
    
    * Fix reference to Org Root in new Account parent_id
    
    * Comment out changes that would require destroy & recreate
    
    To atomize Terraform changes. The next Apply should only create the Users Account.
    
    * Fix ref to parent_id
    
    Cannot refer to parent Org by id. Must use roots[0].id instead

--- workspaces/organization/accounts_management.tf
+++ workspaces/organization/accounts_management.tf
@@ -1,19 +1,29 @@
 # ACCOUNTS
 
-## MANAGEMENT ACCOUNT
+## ROOT MANAGEMENT ACCOUNT
 
 resource "aws_organizations_account" "stem_garden_llc" {
   name  = "STEM Garden LLC"
   email = "nigel@stemgarden.org"
-  #   role_name = "Admin"
-  # management_account
+  # role_name = "Admin"  # Should be set to "Admin" for all new accounts but adding it retroactively necessiutates a destroy & recreate which we want to avoid due to quotas.
 }
 
-#
+## USER & IAM MANAGEMENT ACCOUNT
 
+resource "aws_organizations_account" "user_management_account" {
+  name  = "User Management Account SGLLC"
+  email = "aws+users@stemgarden.org"
 
+  parent_id = aws_organizations_organization.sgllc_org.roots[0].id # This is the default but must be stated to enable drift detection. It's not the Org's id, it's the Root Account's id.
+  close_on_deletion = true # I'm curious if this argument causes immediate deletion or is still subject to the 90 day suspension period before deletion.
+  iam_user_access_to_billing = "ALLOW" # This is the default
+  role_name = "Admin" # The default is OrganizationAccountAccessRole but it is assigned Admin priviledges and this will make assuming the role easier (less typing)
 
-
+  # There is no AWS Organizations API for reading role_name, so the following ignore is required to silence would be inaccurate drift detection.
+  lifecycle {
+    ignore_changes = [role_name]
+  }
+}
 
 # ACCOUNTS
 
--- workspaces/organization/accounts_mm.tf
+++ workspaces/organization/accounts_mm.tf
@@ -5,7 +5,8 @@
 resource "aws_organizations_account" "mm_dev" {
   name      = "Mastery Math Dev"
   email     = "mm-dev@stemgarden.org"
-  parent_id = aws_organizations_organizational_unit.mastery_math.id
+  # parent_id = aws_organizations_organizational_unit.mastery_math.id
+  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
 }
 
 resource "aws_organizations_account" "mastery_math_dev" {
