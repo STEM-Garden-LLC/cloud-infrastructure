924a738c53ed1657bf5107c1082bbba46eb7576d
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-04-02 21:42:43 -0500

    Rename file and fix typo

--- workspaces/user-management/backend.tf
+++ workspaces/user-management/backend.tf
@@ -17,3 +17,44 @@ terraform {
     }
   }
 }
+
+# Default Provider - Un-Aliased
+
+provider "aws" {
+  region = var.region
+
+}
+
+
+# Aliased Providers -- Not really sure which Workspace these belong in yet.
+
+provider "aws" {
+  alias  = "users"
+  region = var.region
+
+  # assume_role {
+  #   role_arn = "arn:aws:iam::${aws_organizations_account.users.id}:role/Admin"
+  # }
+  assume_role {
+    role_arn = "arn:aws:iam::${aws_organizations_account.stem_garden_llc.id}:role/Admin"
+  }
+}
+
+# provider "aws" {
+#   assume_role {
+#     role_arn = "arn:aws:iam::${aws_organizations_account.staging.id}:role/Admin"
+#   }
+
+#   alias  = "staging"
+#   region = var.region
+# }
+
+# provider "aws" {
+#   assume_role {
+#     role_arn = "arn:aws:iam::${aws_organizations_account.production.id}:role/Admin"
+#   }
+
+#   alias  = "production"
+#   region = var.region
+# }
+
