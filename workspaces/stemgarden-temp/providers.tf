eb9049711b45a11d79ed083c4aba65c4451415c9
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-06-22 23:40:36 -0500

    Update providers.tf

--- workspaces/stemgarden-temp/providers.tf
+++ workspaces/stemgarden-temp/providers.tf
@@ -28,7 +28,7 @@ provider "aws" {
 }
 
 // Cloudfront can only use SSL certificates created in us-east-1
-# provider "aws" {
-#   alias = "acm_provider"
-#   region = "us-east-1"
-# }
+provider "aws" {
+  alias = "acm_provider"
+  region = "us-east-1"
+}
