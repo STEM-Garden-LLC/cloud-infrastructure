5db690967bef53ed747c5352a242b87fa45b5cb0
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-06-22 23:03:17 -0500

    Create resources described in linked article
    Article:  https://www.alexhyett.com/terraform-s3-static-website-hosting/
    GitHub:  https://github.com/alexhyett/terraform-s3-static-website/tree/main

---
+++ workspaces/stemgarden-temp/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.4"
+    }
+  }
+
+  cloud {
+    # The following is provided as documentation only. 
+    # This workspace is configured to use the VCS workflow, meaning that this config will be ignored and what is in the workspace settings will actually be in effect. 
+    organization = "steg-garden-llc"
+    hostname = "app.terraform.io" // same as default
+    workspaces {
+      name = "stemgarden-temp"
+    }
+  }
+}
+
+// default provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+// Cloudfront can only use SSL certificates created in us-east-1
+provider "aws" {
+  alias = "acm_provider"
+  region = "us-east-1"
+}
---
+++ workspaces/stemgarden-temp/terraform.tfvars
@@ -0,0 +1,6 @@
+domain_name = "stemgarden.org"
+bucket_name = "stemgarden.org"
+
+common_tags = {
+  Project = "stemgarden.org"
+}
