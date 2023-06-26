fa4fbfa4029751ed92dd0ee74f6e4b4a87c4e67b
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-06-26 07:09:24 -0500

    Uncomment validation_record_fqdns

--- workspaces/stemgarden-temp/acm.tf
+++ workspaces/stemgarden-temp/acm.tf
@@ -17,5 +17,5 @@ resource "aws_acm_certificate" "ssl_certificate" {
 resource "aws_acm_certificate_validation" "cert_validation" {
   provider        = aws.acm_provider
   certificate_arn = aws_acm_certificate.ssl_certificate.arn
-  #validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
