7bfe95dc497379bc62040d4d83adb9a20b84e24d
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-06-25 23:11:22 -0500

    Switch from email to DNS validation

--- workspaces/stemgarden-temp/route53.tf
+++ workspaces/stemgarden-temp/route53.tf
@@ -29,20 +29,20 @@ resource "aws_route53_record" "www-a" {
 }
 
 # Uncomment the below block if you are doing certificate validation using DNS instead of Email.
-#resource "aws_route53_record" "cert_validation" {
-#  for_each = {
-#    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
-#      name    = dvo.resource_record_name
-#      record  = dvo.resource_record_value
-#      type    = dvo.resource_record_type
-#      zone_id = aws_route53_zone.main.zone_id
-#    }
-#  }
-#
-#  allow_overwrite = true
-#  name            = each.value.name
-#  records         = [each.value.record]
-#  ttl             = 60
-#  type            = each.value.type
-#  zone_id         = each.value.zone_id
-#}
+resource "aws_route53_record" "cert_validation" {
+ for_each = {
+   for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+     name    = dvo.resource_record_name
+     record  = dvo.resource_record_value
+     type    = dvo.resource_record_type
+     zone_id = aws_route53_zone.main.zone_id
+   }
+ }
+
+ allow_overwrite = true
+ name            = each.value.name
+ records         = [each.value.record]
+ ttl             = 60
+ type            = each.value.type
+ zone_id         = each.value.zone_id
+}
