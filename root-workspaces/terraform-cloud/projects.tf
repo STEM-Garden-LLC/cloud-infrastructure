9f6a4c21fd99da37faa1f9f4d06c847c1d2439b8
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-07-03 11:48:19 -0500

    Add Projects

--- root-workspaces/terraform-cloud/projects.tf
+++ root-workspaces/terraform-cloud/projects.tf
@@ -1,5 +1,17 @@
 
 resource "tfe_project" "mastery_math" {
-  organization = tfe_organization.sgllc.name
+#   organization = tfe_organization.sgllc.name
   name = "Mastery Math"
 }
+
+resource "tfe_project" "stem_garden_homepage" {
+#   organization = tfe_organization.sgllc.name
+  name = "STEM Garden Homepage"
+}
+
+resource "tfe_project" "chess_club_host" {
+#   organization = tfe_organization.sgllc.name
+  name = "Chess Club Host"
+}
+
+
