0821ad5edae1ba6eb90ffd41c2f57c47d54bb347
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-07-02 21:12:37 -0500

    Add VCS flow to mastery math workspace

--- root-workspaces/terraform-cloud/workspaces.tf
+++ root-workspaces/terraform-cloud/workspaces.tf
@@ -3,5 +3,14 @@
 resource "tfe_workspace" "mastery_math_prod" {
   name         = "mastery-math-prod"
   organization = tfe_organization.sgllc.name
-  tag_names    = ["test", "app"]
+#   project_id = 
+  description = "Production infrastructure for masterymath.org"
+  allow_destroy_plan = false
+  execution_mode = "remote"
+  working_directory = "workspaces/mastery-math-prod"
+  vcs_repo {
+    identifier = "stem-garden-llc/cloud-infrastructure"
+    github_app_installation_id = "ghain-vi62wYKcwTykbwke"
+  }
+
 }
