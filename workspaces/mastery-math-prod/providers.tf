37d44b0497e19b8c9846389a14e5072492225ea9
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-07-02 21:18:05 -0500

    Create providers.tf

---
+++ workspaces/mastery-math-prod/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.4"
+    }
+  }
+
+  cloud {
+    organization = "stem-garden-llc"
+    hostname = "app.terraform.io" 
+    workspaces {
+      name = "mastery-math-prod"
+    }
+  }
+}
+
+// default provider
+// Cloudfront can only use SSL certificates created in us-east-1
+provider "aws" {
+  region = "us-east-1"
+  shared_config_files      = ["/Users/tf_user/.aws/config"]
+  shared_credentials_files = ["/Users/tf_user/.aws/credentials"]
+}
