42be68cfea6d9d0f4733199631d8a64845e43199
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-07-04 16:08:48 -0500

    Revert to DNS validation and remove custom provider for ACM

--- modules/s3-static-site/acm.tf
+++ modules/s3-static-site/acm.tf
@@ -1,6 +1,5 @@
 # # SSL Certificate
 resource "aws_acm_certificate" "ssl_certificate" {
-  provider                  = aws.acm_provider
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
 #   validation_method         = "EMAIL"
@@ -14,8 +13,7 @@ resource "aws_acm_certificate" "ssl_certificate" {
 }
 
 # # Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
-# resource "aws_acm_certificate_validation" "cert_validation" {
-#   provider        = aws.acm_provider
-#   certificate_arn = aws_acm_certificate.ssl_certificate.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
