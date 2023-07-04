e721825cacee2999eaa80d455215cc3fe5a1e742
Nigel Wilson <nigel.wilson@airtonomy.ai>
2023-07-04 15:31:12 -0500

    Add vars.tf to workspace

---
+++ workspaces/mastery-math-prod/vars.tf
@@ -0,0 +1,8 @@
+variable "domain_name" {
+  type        = string
+  description = "The domain name for the website."
+}
+
+variable "common_tags" {
+  description = "Common tags you want applied to all components."
+}
