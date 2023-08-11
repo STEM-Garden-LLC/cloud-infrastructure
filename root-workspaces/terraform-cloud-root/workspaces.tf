2a9134ce32a8f8d685f013c8c7890eac698a53aa
Nigel Wilson <nigel@stemgarden.org>
2023-08-10 21:17:41 -0500

    Comment out workspace for now

--- root-workspaces/terraform-cloud-root/workspaces.tf
+++ root-workspaces/terraform-cloud-root/workspaces.tf
@@ -1,17 +1,17 @@
 
 
-resource "tfe_workspace" "mastery_math_prod" {
-  name         = "mastery-math-prod"
-  organization = tfe_organization.sgllc.name
-  project_id = tfe_project.mastery_math.id
-  description = "Production infrastructure for masterymath.org"
-  allow_destroy_plan = false
-  execution_mode = "remote"
-  working_directory = "/workspaces/mastery-math-prod"
-  trigger_patterns = ["/workspaces/mastery-math-prod/*"]
-  vcs_repo {
-    identifier = "STEM-Garden-LLC/cloud-infrastructure"
-    github_app_installation_id = "ghain-vi62wYKcwTykbwke"
-  }
+# resource "tfe_workspace" "mastery_math_prod" {
+#   name         = "mastery-math-prod"
+#   organization = tfe_organization.sgllc.name
+#   project_id = tfe_project.mastery_math.id
+#   description = "Production infrastructure for masterymath.org"
+#   allow_destroy_plan = false
+#   execution_mode = "remote"
+#   working_directory = "/workspaces/mastery-math-prod"
+#   trigger_patterns = ["/workspaces/mastery-math-prod/*"]
+#   vcs_repo {
+#     identifier = "STEM-Garden-LLC/cloud-infrastructure"
+#     github_app_installation_id = "ghain-vi62wYKcwTykbwke"
+#   }
 
-}
+# }
