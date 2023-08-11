0b39f9a0a3fc8b61e631e4d43a97ecbce1ae6f70
Nigel Wilson <nigel@stemgarden.org>
2023-08-10 22:05:34 -0500

    Add cloud backend config to Terraform-Coud-Root

--- root-workspaces/terraform-cloud-root/providers.tf
+++ root-workspaces/terraform-cloud-root/providers.tf
@@ -1,4 +1,15 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "stem-garden-llc"
+
+    workspaces {
+      name = "terraform-cloud-root"
+    }
+  }
+  
   required_providers {
     tfe = {
       source = "hashicorp/tfe"
