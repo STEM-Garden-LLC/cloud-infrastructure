85bd263d5bb682dbf2b2cfe1fd1db3f36ae4ec58
Nigel Wilson <nigel@stemgarden.org>
2023-08-12 18:13:13 -0500

    Revert "Delete static site resources in mastery math prod"
    This reverts commit 637c06daec6810c8ad3d16e3878d9c4182b694f4.

---
+++ workspaces/mastery-math/prod/s3-static-site.tf
@@ -0,0 +1,11 @@
+module "s3_static_site" {
+  source = "../../modules/s3-static-site"
+
+  domain_name = "masterymath.org"
+  common_tags = {
+    project = "stemgarden.org"
+    provisioned_by = "terraform_cloud"
+  }
+
+}
+
