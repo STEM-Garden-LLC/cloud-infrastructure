26075811c223ea769d9aa5df614c231240eb3e7d
Nigel Wilson <nigel@stemgarden.org>
2023-08-12 18:21:28 -0500

    Rearrange hosted zone import

--- workspaces/mastery-math/prod/hosted_zone.tf
+++ workspaces/mastery-math/prod/hosted_zone.tf
@@ -12,7 +12,15 @@ import {
   id = "Z06677023RZS93Q99ME56"
 }
 
-resource "aws_route53_zone" "mastery_math" {
-  name = local.domain_name
-  tags = local.common_tags
-}
+# resource "aws_route53_zone" "mastery_math" {
+#   name = local.domain_name
+#   name_servers = [
+#     "ns-1455.awsdns-53.org",
+#     "ns-1875.awsdns-42.co.uk",
+#     "ns-371.awsdns-46.com",
+#     "ns-602.awsdns-11.net"
+#   ]
+
+
+#   tags = local.common_tags
+# }
--- workspaces/mastery-math/prod/s3-static-site.tf
+++ workspaces/mastery-math/prod/s3-static-site.tf
@@ -1,11 +1,15 @@
-module "s3_static_site" {
-  source = "../../modules/s3-static-site"
+# module "s3_static_site" {
+#   source = "../../modules/s3-static-site"
 
-  domain_name = "masterymath.org"
-  common_tags = {
-    project = "stemgarden.org"
-    provisioned_by = "terraform_cloud"
-  }
+#   domain_name = "masterymath.org"
+#   common_tags = {
+#     project = "stemgarden.org"
+#     provisioned_by = "terraform_cloud"
+#   }
 
+# }
+
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+  tags = var.common_tags
 }
-
