cea7c48c9ae16b8190144ec1f954d8a98abbb042
Nigel Wilson <nigel@stemgarden.org>
2023-08-12 18:05:24 -0500

    Attempt using import block

--- workspaces/mastery-math/prod/hosted_zone.tf
+++ workspaces/mastery-math/prod/hosted_zone.tf
@@ -7,6 +7,11 @@ locals {
     }
 }
 
+import {
+  to = aws_route53_zone.mastery_math
+  id = "Z06677023RZS93Q99ME56"
+}
+
 resource "aws_route53_zone" "mastery_math" {
   name = local.domain_name
   tags = local.common_tags
