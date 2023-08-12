9fb2842698995b5d34f55c209e37c5b49633fab7
Nigel Wilson <nigel@stemgarden.org>
2023-08-12 18:43:33 -0500

    Rearrange for HZ import

--- modules/s3-static-site/acm.tf
+++ modules/s3-static-site/acm.tf
@@ -1,19 +1,19 @@
-# # SSL Certificate
-resource "aws_acm_certificate" "ssl_certificate" {
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-#   validation_method         = "EMAIL"
-  validation_method         = "DNS"
+# # # SSL Certificate
+# resource "aws_acm_certificate" "ssl_certificate" {
+#   domain_name               = var.domain_name
+#   subject_alternative_names = ["*.${var.domain_name}"]
+# #   validation_method         = "EMAIL"
+#   validation_method         = "DNS"
 
-  tags = var.common_tags
+#   tags = var.common_tags
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-# # Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# # # Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   certificate_arn = aws_acm_certificate.ssl_certificate.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
--- workspaces/mastery-math/prod/s3-static-site.tf
+++ workspaces/mastery-math/prod/s3-static-site.tf
@@ -1,13 +1,13 @@
-# module "s3_static_site" {
-#   source = "../../modules/s3-static-site"
+module "s3_static_site" {
+  source = "../../modules/s3-static-site"
 
-#   domain_name = "masterymath.org"
-#   common_tags = {
-#     project = "stemgarden.org"
-#     provisioned_by = "terraform_cloud"
-#   }
+  domain_name = "masterymath.org"
+  common_tags = {
+    project = "stemgarden.org"
+    provisioned_by = "terraform_cloud"
+  }
 
-# }
+}
 
 variable "domain_name" {
   type        = string
@@ -19,7 +19,7 @@ variable "common_tags" {
 }
 
 
-resource "aws_route53_zone" "main" {
-  name = var.domain_name
-  tags = var.common_tags
-}
+# resource "aws_route53_zone" "main" {
+#   name = var.domain_name
+#   tags = var.common_tags
+# }
