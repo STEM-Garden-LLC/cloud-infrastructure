db7a8507c8c090cc5418073fa09f4ffd7f8199b8
Nigel Wilson <nigel@stemgarden.org>
2023-08-12 18:31:23 -0500

    Delete resources but not config from sandbox

--- workspaces/sandbox/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_iam_policy_document" "assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
