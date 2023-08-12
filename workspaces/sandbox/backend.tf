60ac0d642cece523746bbeb59c84b3f398ac277b
Nigel Wilson <nigel@stemgarden.org>
2023-08-12 18:29:12 -0500

    Revert "Empty out sandbox workspace"
    This reverts commit e67329bca370e1bca0efc7e20d09d0c36c79f0e8.

---
+++ workspaces/sandbox/backend.tf
@@ -0,0 +1,27 @@
+terraform {
+    # required_version = "~> 1.3.1" # pessimistic
+    required_version = ">= 1.3.1" # optomistic
+
+    cloud {
+        hostname = "app.terraform.io"
+        organization = "stem-garden-llc"
+
+        workspaces {
+            name = "sandbox"
+            # tags = ["source:cli"]
+        }
+    }
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 4.47.0"
+        }
+    }
+}
+
+provider "aws" {
+  region = var.region
+
+}
+
---
+++ workspaces/sandbox/data.tf
@@ -0,0 +1,9 @@
+data "aws_iam_policy_document" "assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
