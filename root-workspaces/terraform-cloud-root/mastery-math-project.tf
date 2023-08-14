6355f11710be124c5550aa337cd99b83ccfbecb5
Nigel Wilson <nigel@stemgarden.org>
2023-08-14 12:36:02 -0500

    Add locally imported mastery-math-old workspace to config

--- root-workspaces/terraform-cloud-root/mastery-math-project.tf
+++ root-workspaces/terraform-cloud-root/mastery-math-project.tf
@@ -4,3 +4,7 @@ module "mastery_math_project_and_workspaces" {
   project_name   = "mastery-math"
   workspace_list = ["dev", "stage", "prod", "shared"]
 }
+
+resource "tfe_workspace" "mastery_math_old" {
+  name = "mastery-math-old"
+}
