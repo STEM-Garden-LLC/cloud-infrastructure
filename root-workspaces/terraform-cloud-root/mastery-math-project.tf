ab775653ca3887a5a77fae1fc4f1ee1a804a7609
Nigel Wilson <nigel@stemgarden.org>
2023-08-14 12:39:35 -0500

    Flesh out imported resource config to remove unwanted changes from plan

--- root-workspaces/terraform-cloud-root/mastery-math-project.tf
+++ root-workspaces/terraform-cloud-root/mastery-math-project.tf
@@ -7,4 +7,17 @@ module "mastery_math_project_and_workspaces" {
 
 resource "tfe_workspace" "mastery_math_old" {
   name = "mastery-math-old"
+  description = "Production infrastructure hosting masterymath.org generated with stand alone resources prior to creation of the project-with-workspaces module."
+
+  working_directory = "workspaces/mastery-math/old"
+
+  file_triggers_enabled = true
+  queue_all_runs        = true
+  force_delete          = false
+
+  vcs_repo {
+    github_app_installation_id = "ghain-vi62wYKcwTykbwke"
+    identifier                 = "STEM-Garden-LLC/cloud-infrastructure"
+    ingress_submodules         = false
+  }
 }
