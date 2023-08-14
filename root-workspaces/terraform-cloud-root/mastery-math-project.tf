7cc59d63e110d745abee81d846740c94b9693246
Nigel Wilson <nigel@stemgarden.org>
2023-08-14 13:01:45 -0500

    Comment out module to fix temporary name conflict

--- root-workspaces/terraform-cloud-root/mastery-math-project.tf
+++ root-workspaces/terraform-cloud-root/mastery-math-project.tf
@@ -1,9 +1,9 @@
-module "mastery_math_project_and_workspaces" {
-  source = "../../modules/project-with-workspaces"
+# module "mastery_math_project_and_workspaces" {
+#   source = "../../modules/project-with-workspaces"
 
-  project_name   = "mastery-math"
-  workspace_list = ["dev", "stage", "prod", "shared"]
-}
+#   project_name   = "mastery-math"
+#   workspace_list = ["dev", "stage", "prod", "shared"]
+# }
 
 resource "tfe_workspace" "mastery_math_old" {
   name = "mastery-math-old"
