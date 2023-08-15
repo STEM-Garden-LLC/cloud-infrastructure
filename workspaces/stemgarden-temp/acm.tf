37819729a60d8c36aa4d3f353ebb17fee57ee1c7
Nigel Wilson <nigel@stemgarden.org>
2023-08-15 15:30:16 -0500

    RM resources from management in prep to delete stemgarden temp wksp

--- workspaces/stemgarden-temp/acm.tf
+++ workspaces/stemgarden-temp/acm.tf
@@ -1,17 +1,17 @@
-# SSL Certificate
-resource "aws_acm_certificate" "ssl_certificate" {
-  provider                  = aws.acm_provider
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-#   validation_method         = "EMAIL"
-  validation_method         = "DNS"
+# # SSL Certificate
+# resource "aws_acm_certificate" "ssl_certificate" {
+#   provider                  = aws.acm_provider
+#   domain_name               = var.domain_name
+#   subject_alternative_names = ["*.${var.domain_name}"]
+# #   validation_method         = "EMAIL"
+#   validation_method         = "DNS"
 
-  tags = var.common_tags
+#   tags = var.common_tags
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
 # Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
 resource "aws_acm_certificate_validation" "cert_validation" {
