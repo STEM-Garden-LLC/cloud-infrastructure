fefaf3964aaa59d32ea4565a606867211feb6b11
Nigel Wilson <nigel@stemgarden.org>
2023-08-15 09:45:14 -0500

    Create Sandbox project with module

---
+++ root-workspaces/terraform-cloud-root/sandbox-project.tf
@@ -0,0 +1,11 @@
+module "mastery_math_project_and_workspaces" {
+  source = "../../modules/project-with-workspaces"
+
+  project_name   = "sandbox"
+  workspace_list = [
+    "s3_bucket_github_actions",
+    "static_site_s3_cf_r53",
+    "next_on_amplify",
+    "node_on_ec2"
+  ]
+}
