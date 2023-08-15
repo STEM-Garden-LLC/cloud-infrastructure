63abb0b154b0426bf2f9e6f91823ea32a0f5bbde
Nigel Wilson <nigel@stemgarden.org>
2023-08-15 10:17:00 -0500

    Add aws creds to provider

--- workspaces/sandbox/github-action-push-to-s3/providers.tf
+++ workspaces/sandbox/github-action-push-to-s3/providers.tf
@@ -1,27 +1,28 @@
 terraform {
-    # required_version = "~> 1.3.1" # pessimistic
-    required_version = ">= 1.3.1" # optomistic
+  required_version = "~> 1.3"
 
-    cloud {
-        hostname = "app.terraform.io"
-        organization = "stem-garden-llc"
-
-        workspaces {
-            name = "sandbox-s3-bucket-github-actions"
-            # tags = ["source:cli"]
-        }
+  cloud {
+    hostname = "app.terraform.io" 
+    organization = "stem-garden-llc"
+    
+    workspaces {
+      name = "sandbox-github-action-push-to-s3"
     }
+  }
 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = ">= 4.47.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    #   version = ">= 4.47.0"
+      version = "~> 5.4"
     }
+  }
 }
 
+// default provider
+// Cloudfront can only use SSL certificates created in us-east-1
 provider "aws" {
   region = "us-east-1"
-
+  shared_config_files      = ["/Users/tf_user/.aws/config"]
+  shared_credentials_files = ["/Users/tf_user/.aws/credentials"]
 }
-
