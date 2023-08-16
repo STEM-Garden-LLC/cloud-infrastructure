d5a7911632adc7fada0eb243d42b8341fb57bd34
Nigel Wilson <nigel@stemgarden.org>
2023-08-15 23:53:48 -0500

    Comment out aws_acm_certificate_validation
    Trying to fix Error: Reference to undeclared resource
    on acm.tf line 19, in resource "aws_acm_certificate_validation" "cert_validation":

--- workspaces/stemgarden-temp/acm.tf
+++ workspaces/stemgarden-temp/acm.tf
@@ -14,8 +14,8 @@
 # }
 
 # Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
-resource "aws_acm_certificate_validation" "cert_validation" {
-  provider        = aws.acm_provider
-  certificate_arn = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   provider        = aws.acm_provider
+#   certificate_arn = aws_acm_certificate.ssl_certificate.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
