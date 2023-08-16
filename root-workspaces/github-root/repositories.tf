2ce335fc81adfe6818904279f7094d370bfac408
Nigel Wilson <nigel@stemgarden.org>
2023-08-16 14:54:26 -0500

    Create stub to import vite test repo

---
+++ root-workspaces/github-root/repositories.tf
@@ -0,0 +1,10 @@
+
+# React with Vite Test
+resource "github_repository" "react_with_vite_test" {
+  name        = "react-with-vite-test"
+  description = "Simple React starter app with Vite dev server and build system."
+
+  visibility = "public"
+
+  
+}
