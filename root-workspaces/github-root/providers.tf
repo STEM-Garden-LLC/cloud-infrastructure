cb90602b638e337ee21a9444b4cffc6f145c891f
Nigel Wilson <nigel@stemgarden.org>
2023-08-15 22:12:28 -0500

    Add Org provider as default
    And make personal account provider alias

--- root-workspaces/github-root/providers.tf
+++ root-workspaces/github-root/providers.tf
@@ -3,32 +3,53 @@ terraform {
     github = {
       source  = "integrations/github"
       version = "~> 5.0"
+      configuration_aliases = [ github.personal ]
     }
   }
 }
 
-# Configure the GitHub Provider
-provider "github" {
-#   owner = "stem-garden-llc"
-  owner = "nigel-f-wilson"
-  token = var.github_token
 
-#   app_auth {
-#     # id              = var.app_id              # or `GITHUB_APP_ID`
-#     # installation_id = var.app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
-#     # pem_file        = var.app_pem_file        # or `GITHUB_APP_PEM_FILE`
-#   }
+
+# Default
+# Configure the GitHub Provider to provision resources in stem-garden-llc account
+variable "github_app_id" {
+  type = string
 }
 
-# Add a user to the organization
-# resource "github_membership" "membership_for_user_x" {
-#   # ...
-# }
+variable "github_app_installation_id" {
+  type = string
+}
 
+variable "github_app_pem_file" {
+  type = string
+}
+
+provider "github" {
+  owner = "stem-garden-llc"
+
+  app_auth {
+    id              = var.github_app_id              # or `GITHUB_APP_ID`
+    installation_id = var.github_app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
+    pem_file        = var.github_app_pem_file        # or `GITHUB_APP_PEM_FILE`
+  }
+}
+
+# Must Specify to use this provider
+# Configure the GitHub Provider to provision resources in nigel-f-wilson user account
 variable "github_token" {
   type = string
 }
 
+provider "github" {
+  alias = "personal"
+
+  owner = "nigel-f-wilson"
+  token = var.github_token
+}
+
+
+
+# Test Resource
 resource "github_repository" "tfc_test_repo" {
   name        = "TFC-test-repo"
   description = "My awesome codebase"
