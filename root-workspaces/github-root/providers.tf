be769ac9f8ab62fedcedebe59df58098ce74386b
Nigel Wilson <nigel@stemgarden.org>
2023-08-15 23:43:37 -0500

    Remove test repo in personal account

--- root-workspaces/github-root/providers.tf
+++ root-workspaces/github-root/providers.tf
@@ -39,16 +39,16 @@ variable "github_token" {
   type = string
 }
 
-provider "github" {
-  owner = "stem-garden-llc"
-  token = var.github_token
-}
+# provider "github" {
+#   owner = "stem-garden-llc"
+#   token = var.github_token
+# }
 
 
 # Must Specify to use this provider
 # Configure the GitHub Provider to provision resources in nigel-f-wilson user account
 provider "github" {
-  alias = "personal"
+#   alias = "personal"
 
   owner = "nigel-f-wilson"
   token = var.github_token
@@ -58,16 +58,16 @@ provider "github" {
 
 
 # Test Resource
-resource "github_repository" "tfc_test_repo" {
-  name        = "TFC-test-repo"
-  description = "My awesome codebase"
+# resource "github_repository" "tfc_test_repo" {
+#   name        = "TFC-test-repo"
+#   description = "My awesome codebase"
 
-  visibility = "public"
-  has_issues = true
-#   has_discussions = true
-#   has_projects = true
-  allow_squash_merge = true
-  allow_rebase_merge = true
+#   visibility = "public"
+#   has_issues = true
+# #   has_discussions = true
+# #   has_projects = true
+#   allow_squash_merge = true
+#   allow_rebase_merge = true
 
 
-}
+# }
