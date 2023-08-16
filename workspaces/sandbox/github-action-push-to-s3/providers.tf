5fb5955a4f15be681922297a499e7f30aa8f3c6a
Nigel Wilson <nigel@stemgarden.org>
2023-08-16 16:08:38 -0500

    Add github provider to sandbox workspace

--- workspaces/sandbox/github-action-push-to-s3/providers.tf
+++ workspaces/sandbox/github-action-push-to-s3/providers.tf
@@ -13,16 +13,26 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-    #   version = ">= 4.47.0"
       version = "~> 5.4"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+      configuration_aliases = [ github.personal ]
     }
   }
 }
 
-// default provider
+// Default AWS provider
 // Cloudfront can only use SSL certificates created in us-east-1
 provider "aws" {
   region = "us-east-1"
   shared_config_files      = ["/Users/nigelwilson/.aws/config"]
   shared_credentials_files = ["/Users/nigelwilson/.aws/credentials"]
+}
+
+# Default GitHub Provider
+# Configure the GitHub Provider to provision resources in stem-garden-llc account
+provider "github" {
+  owner = "stem-garden-llc"
 }
