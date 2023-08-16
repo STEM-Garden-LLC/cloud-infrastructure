f564f68b742b6c322e856e29c87db35ce545d0a4
Nigel Wilson <nigel@stemgarden.org>
2023-08-16 14:28:50 -0500

    Add fake domain name to sandbox-project

--- root-workspaces/terraform-cloud-root/sandbox-project.tf
+++ root-workspaces/terraform-cloud-root/sandbox-project.tf
@@ -2,6 +2,7 @@ module "sandbox_project_and_workspaces" {
   source = "../../modules/project-with-workspaces"
 
   project_name   = "sandbox"
+  domain_name    = "example.com"
   workspace_list = [
     "github-action-push-to-s3",
     "static-site-s3-cf-r53",
