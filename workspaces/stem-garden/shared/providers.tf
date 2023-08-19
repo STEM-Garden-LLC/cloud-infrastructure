54e73df6464f677148b7b88d798c5b1ca164d143
Nigel Wilson <nigel@stemgarden.org>
2023-08-19 15:16:49 -0500

    Create workspace folders in stem-garden project

---
+++ workspaces/stem-garden/shared/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.4"
+    }
+  }
+
+  cloud {
+    organization = "stem-garden-llc"
+    hostname = "app.terraform.io" 
+    workspaces {
+      name = "stem-garden-shared"
+    }
+  }
+}
+
+// default provider
+// Cloudfront can only use SSL certificates created in us-east-1
+provider "aws" {
+  region = "us-east-1"
+  shared_config_files      = ["/Users/nigelwilson/.aws/config"]
+  shared_credentials_files = ["/Users/nigelwilson/.aws/credentials"]
+}
