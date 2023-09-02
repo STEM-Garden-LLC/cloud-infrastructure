c1ce4006fa559b866af58221345d0cb850501616
Nigel Wilson <nigel@stemgarden.org>
2023-09-01 23:38:00 -0400

    Add preexisting bucket bool

--- modules/s3-static-site/vars.tf
+++ modules/s3-static-site/vars.tf
@@ -3,6 +3,16 @@ variable "domain_name" {
   description = "The domain name for the website."
 }
 
+variable "use_preexisting_bucket" {
+  type        = bool
+  description = "Whether to treat the bucket_name as the name of an existing bucket or to create a new bucket with that name."
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket to serve website contents from."
+}
+
 variable "common_tags" {
   description = "Common tags you want applied to all components."
 }
