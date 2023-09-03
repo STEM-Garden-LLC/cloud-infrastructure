9bcb14909b3427ed72484d2a6ddc52ce820924ad
Nigel Wilson <nigel@stemgarden.org>
2023-09-03 18:00:32 -0400

    Delete MM Old workspace

--- root-workspaces/terraform-cloud-root/mastery-math-project.tf
+++ root-workspaces/terraform-cloud-root/mastery-math-project.tf
@@ -5,20 +5,3 @@ module "mastery_math_project_and_workspaces" {
   domain_name    = "masterymath.org"
   workspace_list = ["dev", "stage", "prod", "shared"]
 }
-
-resource "tfe_workspace" "mastery_math_old" {
-  name        = "mastery-math-old"
-  description = "Production infrastructure hosting masterymath.org generated with stand alone resources prior to creation of the project-with-workspaces module."
-
-  working_directory = "workspaces/mastery-math/old"
-
-  file_triggers_enabled = true
-  queue_all_runs        = true
-  force_delete          = false
-
-  vcs_repo {
-    github_app_installation_id = "ghain-vi62wYKcwTykbwke"
-    identifier                 = "STEM-Garden-LLC/cloud-infrastructure"
-    ingress_submodules         = false
-  }
-}
