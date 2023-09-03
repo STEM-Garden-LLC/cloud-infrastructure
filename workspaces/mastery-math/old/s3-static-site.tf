e1b1733afa556779030a9c8b76a341f778a29f23
Nigel Wilson <nigel@stemgarden.org>
2023-09-02 23:25:28 -0400

    Add missing required vars

--- workspaces/mastery-math/old/s3-static-site.tf
+++ workspaces/mastery-math/old/s3-static-site.tf
@@ -2,6 +2,10 @@ module "s3_static_site" {
   source = "../../../modules/s3-static-site"
 
   domain_name = "masterymath.org"
+
+  use_preexisting_bucket = true
+  bucket_name = "masterymath.org"
+
   common_tags = {
     project = "stemgarden.org"
     provisioned_by = "terraform_cloud"
