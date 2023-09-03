c323effeba899458a8d0da847caa9cc1527d77f9
Nigel Wilson <nigel@stemgarden.org>
2023-09-02 21:58:15 -0400

    Remove multi-layering of OUs

--- workspaces/organization/organization.tf
+++ workspaces/organization/organization.tf
@@ -9,35 +9,17 @@ resource "aws_organizations_organization" "sgllc_org" {
   #   ]
 }
 
-# ORGANIZATIONAL UNITS -- LAYER 1
-
-resource "aws_organizations_organizational_unit" "projects" {
-  name      = "Projects"
-  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
-}
-
-resource "aws_organizations_organizational_unit" "sandbox" {
-  name      = "Sandbox"
-  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
-}
-
-# PROJECTS OU -- LAYER 2
+# ORGANIZATIONAL UNITS
 
 resource "aws_organizations_organizational_unit" "mastery_math" {
   name      = "Mastery Math"
-  parent_id = aws_organizations_organizational_unit.projects.id
+  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "stem_garden_homepage" {
   name      = "STEM Garden Homepage"
-  parent_id = aws_organizations_organizational_unit.projects.id
+  parent_id = aws_organizations_organization.sgllc_org.roots[0].id
 }
 
-# SANDBOX OU -- LAYER 2
-
-resource "aws_organizations_organizational_unit" "key_cloak" {
-  name      = "KeyCloak"
-  parent_id = aws_organizations_organizational_unit.sandbox.id
-}
 
 
