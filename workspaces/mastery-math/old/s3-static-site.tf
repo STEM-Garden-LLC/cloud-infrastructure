116e5dc3e82de1450bad4d450fa83ae6a235bfc4
Nigel Wilson <nigel@stemgarden.org>
2023-09-03 13:49:23 -0400

    Comment out module call in MM old

--- workspaces/mastery-math/old/s3-static-site.tf
+++ workspaces/mastery-math/old/s3-static-site.tf
@@ -1,26 +1,26 @@
-module "s3_static_site" {
-  source = "../../../modules/s3-static-site"
+# module "s3_static_site" {
+#   source = "../../../modules/s3-static-site"
 
-  domain_name = "masterymath.org"
+#   domain_name = "masterymath.org"
 
-  use_preexisting_bucket = false
-  bucket_name = "masterymath.org"
+#   use_preexisting_bucket = false
+#   bucket_name = "masterymath.org"
 
-  common_tags = {
-    project = "stemgarden.org"
-    provisioned_by = "terraform_cloud"
-  }
+#   common_tags = {
+#     project = "stemgarden.org"
+#     provisioned_by = "terraform_cloud"
+#   }
 
-}
+# }
 
-variable "domain_name" {
-  type        = string
-  description = "The domain name for the website."
-}
+# variable "domain_name" {
+#   type        = string
+#   description = "The domain name for the website."
+# }
 
-variable "common_tags" {
-  description = "Common tags you want applied to all components."
-}
+# variable "common_tags" {
+#   description = "Common tags you want applied to all components."
+# }
 
 
 # resource "aws_route53_zone" "main" {
