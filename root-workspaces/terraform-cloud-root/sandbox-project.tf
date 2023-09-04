33c7d0ef397f708b2ad6954c3cd244af8500456f
Nigel Wilson <nigel@stemgarden.org>
2023-09-04 12:37:47 -0400

    Re-add workspace after delete failed
    due to the fact the workspace contains active resources.

--- root-workspaces/terraform-cloud-root/sandbox-project.tf
+++ root-workspaces/terraform-cloud-root/sandbox-project.tf
@@ -4,7 +4,7 @@ module "sandbox_project_and_workspaces" {
   project_name   = "sandbox"
   domain_name    = "example.com"
   workspace_list = [
-    # "github-action-push-to-s3",
+    "github-action-push-to-s3",
     # "static-site-s3-cf-r53",
     # "next-on-amplify",
     # "node-on-ec2",
