903664c40064dbac01ab160d27838371094d277e
Nigel Wilson <nigel@stemgarden.org>
2023-09-04 13:39:00 -0400

    Convert bucket name to a local based on tfc project and workspace names

--- modules/s3-static-site/vars.tf
+++ modules/s3-static-site/vars.tf
@@ -18,11 +18,15 @@ variable "use_preexisting_bucket" {
   description = "Whether to treat the bucket_name as the name of an existing bucket or to create a new bucket with that name."
 }
 
-variable "bucket_name" {
-  type        = string
-  description = "The name of the S3 bucket to serve website contents from."
-}
+# variable "bucket_name" {
+#   type        = string
+#   description = "The name of the S3 bucket to serve website contents from."
+# }
 
 variable "common_tags" {
   description = "Common tags you want applied to all components."
+}
+
+locals {
+  bucket_name = "${var.tfc_project}-${var.tfc_workspace}"
 }
