55a557749e242f56942e09cbe479d030ce9d4c17
Nigel Wilson <nigel@stemgarden.org>
2023-09-04 12:34:12 -0400

    Update workspaces in the sandbox project

--- root-workspaces/terraform-cloud-root/sandbox-project.tf
+++ root-workspaces/terraform-cloud-root/sandbox-project.tf
@@ -4,9 +4,10 @@ module "sandbox_project_and_workspaces" {
   project_name   = "sandbox"
   domain_name    = "example.com"
   workspace_list = [
-    "github-action-push-to-s3",
-    "static-site-s3-cf-r53",
-    "next-on-amplify",
-    "node-on-ec2"
+    # "github-action-push-to-s3",
+    # "static-site-s3-cf-r53",
+    # "next-on-amplify",
+    # "node-on-ec2",
+    "react-with-vite-test"
   ]
 }
