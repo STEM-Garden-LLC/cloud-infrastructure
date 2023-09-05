5a5f657151f54202b2f02343845cdd2a55923bf6
Nigel Wilson <nigel@stemgarden.org>
2023-09-04 23:39:59 -0400

    comment out ACM cert validation resource

--- modules/s3-static-site/acm.tf
+++ modules/s3-static-site/acm.tf
@@ -13,7 +13,7 @@ resource "aws_acm_certificate" "ssl_certificate" {
 }
 
 # Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   certificate_arn = aws_acm_certificate.ssl_certificate.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
