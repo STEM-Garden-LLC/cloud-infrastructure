7627fb32647c4035783ce54e25681f5ac5741e7c
Nigel Wilson <nigel@stemgarden.org>
2023-09-04 21:58:23 -0400

    Fix ref to CF distribution

--- modules/s3-static-site/route53.tf
+++ modules/s3-static-site/route53.tf
@@ -22,8 +22,8 @@ resource "aws_route53_record" "root-a" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
