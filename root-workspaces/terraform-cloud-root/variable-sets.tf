95a59d2d7c885acaeb1c0625a28cdd359e6e0cdd
Nigel Wilson <nigel@stemgarden.org>
2023-09-06 14:20:05 -0400

    Make token sensitive

--- root-workspaces/terraform-cloud-root/variable-sets.tf
+++ root-workspaces/terraform-cloud-root/variable-sets.tf
@@ -16,6 +16,7 @@ resource "tfe_variable" "gh_personal_access_token" {
   description     = "nigel-f-wilson's personal access token"
   key             = "GITHUB_TOKEN"
   value           = var.github_token
+  sensitive       = true
   category        = "env"
   variable_set_id = tfe_variable_set.gh_credentials.id
 }
