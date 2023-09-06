e04ac4bdee95c88a8a2147f5af1e69205eab50fb
Nigel Wilson <nigel@stemgarden.org>
2023-09-06 18:25:48 -0400

    Prepare imports of apex Hosted Zones

---
+++ root-workspaces/aws-root/dns.tf
@@ -0,0 +1,14 @@
+import {
+  to = aws_route53_zone.com_chessclubhost
+  id = "Z0709817284VP6KJ4AIUQ"
+}
+
+import {
+  to = aws_route53_zone.org_masterymath
+  id = "Z06677023RZS93Q99ME56"
+}
+
+import {
+  to = aws_route53_zone.org_stemgarden
+  id = "Z04644452DUB6MD4Z75A7"
+}
