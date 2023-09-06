33f4e15733cd7716764dbe46b6b5becc082c8a8b
Nigel Wilson <nigel@stemgarden.org>
2023-09-06 10:14:27 -0400

    Add generated hosted zone code for import

--- workspaces/mastery-math/shared/dns.tf
+++ workspaces/mastery-math/shared/dns.tf
@@ -3,3 +3,14 @@ import {
   to = aws_route53_zone.mastery_math
   id = "Z06677023RZS93Q99ME56"
 }
+
+resource "aws_route53_zone" "mastery_math" {
+  comment           = "Managed by TFC"
+  delegation_set_id = null
+  force_destroy     = null
+  name              = "masterymath.org"
+  tags = {
+    project        = "stemgarden.org"
+    provisioned_by = "terraform_cloud"
+  }
+}
