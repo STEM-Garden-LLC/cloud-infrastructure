a4a3ae24348184ff1c046e1d9a978d578baf6e71
Nigel Wilson <nigel@stemgarden.org>
2023-09-06 14:41:29 -0400

    Rename gh_creds var set and import app_registration

--- root-workspaces/terraform-cloud-root/variable-sets.tf
+++ root-workspaces/terraform-cloud-root/variable-sets.tf
@@ -3,7 +3,7 @@
 ##############
 
 resource "tfe_variable_set" "gh_credentials" {
-  name          = "Test Varset"
+  name          = "GitHub Credentials"
   description   = "Some description."
   organization  = tfe_organization.sgllc.name
 }
@@ -21,37 +21,11 @@ resource "tfe_variable" "gh_personal_access_token" {
   variable_set_id = tfe_variable_set.gh_credentials.id
 }
 
-# The following vars were manually created in the Variable set: GitHub Organization App Registration
-# I added these stubs while planning to import them but realized it was not a priority. 
-# I got stuck trying to find the VARIABLE_ID required by the import command.
-# 
-# resource "tfe_variable" "gh_app_id" {
-#   description     = "From the URL of the OAuth App in the Org"
-#   key             = "github_app_id"
-# #   value           = var.github_token
-#   sensitive       = true
-#   category        = "terraform"
-# #   variable_set_id = tfe_variable_set.gh_credentials.id
-# }
-
-# resource "tfe_variable" "gh_app_installation_id" {
-#   description     = "The Client ID from the OAuth App's settings"
-#   key             = "github_app_id"
-# #   value           = var.github_token
-#   sensitive       = true
-#   category        = "terraform"
-# #   variable_set_id = tfe_variable_set.gh_credentials.id
-# }
-
-# resource "tfe_variable" "gh_app_pem_file" {
-#   description     = "The Client Secret stored in Apple Passwords manager under github-oauth-app-for-tfc.com"
-#   key             = "github_app_id"
-# #   value           = var.github_token
-#   sensitive       = true
-#   category        = "terraform"
-# #   variable_set_id = tfe_variable_set.gh_credentials.id
-# }
-
+resource "tfe_variable_set" "gh_org_app_registration" {
+  name          = "GitHub TFC App Installation"
+  description   = "The Terraform Cloud App that is installed in the STEM Garden LLC GitHub Org."
+  organization  = tfe_organization.sgllc.name
+}
 
 
 ###########
