38f451a14ae02f35bdf971a5974bafe4a22e9b71
Nigel Wilson <nigel@stemgarden.org>
2023-09-06 15:09:47 -0400

    Start managing var set to project associations with TF

---
+++ root-workspaces/terraform-cloud-root/var-set-associations.tf
@@ -0,0 +1,13 @@
+######################################
+##   GitHub Personal Access Token   ##
+######################################
+
+resource "tfe_workspace_variable_set" "gh_pat_to_root" {
+  variable_set_id = tfe_variable_set.gh_credentials.id
+  workspace_id    = tfe_workspace.github_root.id
+}
+
+resource "tfe_project_variable_set" "gh_pat_to_sandbox" {
+  variable_set_id = tfe_variable_set.gh_credentials.id
+  project_id    = module.sandbox_project_and_workspaces.project_name
+}
