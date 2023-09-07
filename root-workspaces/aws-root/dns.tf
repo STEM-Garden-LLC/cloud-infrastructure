a8a7fa33dc4a76011903678e969a216e47dae91b
Nigel Wilson <nigel@stemgarden.org>
2023-09-07 15:07:46 -0400

    Reorganize file

--- root-workspaces/aws-root/dns.tf
+++ root-workspaces/aws-root/dns.tf
@@ -1,17 +1,45 @@
-import {
-  to = aws_route53_zone.com_chessclubhost
-  id = "Z0709817284VP6KJ4AIUQ"
+##########################
+##       Sandbox        ##
+##########################
+
+
+##########################
+##     Mastery Math     ##
+##########################
+
+resource "aws_route53_zone" "org_masterymath" {
+  comment           = "Mastery Math"
+  delegation_set_id = null
+  force_destroy     = null
+  name              = "masterymath.org"
+
+  tags = {
+    project        = "Mastery Math"
+    provisioned_by = "Terraform Cloud"
+    workspace      = "AWS Root"
+  }
 }
 
-import {
-  to = aws_route53_zone.org_masterymath
-  id = "Z06677023RZS93Q99ME56"
+##########################
+##      STEM Garden     ##
+##########################
+
+resource "aws_route53_zone" "org_stemgarden" {
+  comment           = "STEM Garden"
+  delegation_set_id = null
+  force_destroy     = null
+  name              = "stemgarden.org"
+  
+  tags = {
+    project        = "STEM Garden"
+    provisioned_by = "Terraform Cloud"
+    workspace      = "AWS Root"
+  }
 }
 
-import {
-  to = aws_route53_zone.org_stemgarden
-  id = "Z04644452DUB6MD4Z75A7"
-}
+##########################
+##   Chess Club Host    ##
+##########################
 
 resource "aws_route53_zone" "com_chessclubhost" {
   comment           = "HostedZone created by Route53 Registrar"
@@ -20,34 +48,4 @@ resource "aws_route53_zone" "com_chessclubhost" {
   name              = "chessclubhost.com"
   tags              = {}
   tags_all          = {}
-}
-
-resource "aws_route53_zone" "org_masterymath" {
-  comment           = "Managed by TFC"
-  delegation_set_id = null
-  force_destroy     = null
-  name              = "masterymath.org"
-  tags = {
-    project        = "Mastery Math"
-    provisioned_by = "Terraform Cloud"
-    workspace      = "Shared"
-  }
-  tags_all = {
-    project        = "Mastery Math"
-    provisioned_by = "Terraform Cloud"
-    workspace      = "Shared"
-  }
-}
-
-resource "aws_route53_zone" "org_stemgarden" {
-  comment           = "Managed by Terraform"
-  delegation_set_id = null
-  force_destroy     = null
-  name              = "stemgarden.org"
-  tags = {
-    project = "stemgarden.org"
-  }
-  tags_all = {
-    project = "stemgarden.org"
-  }
 }
