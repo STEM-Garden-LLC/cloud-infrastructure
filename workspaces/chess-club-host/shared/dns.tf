d48d403c0fb5ea68bccd9af486ea0e66c35b6c11
Nigel Wilson <nigel@stemgarden.org>
2023-09-07 17:28:46 -0400

    Add config for imports

--- workspaces/chess-club-host/shared/dns.tf
+++ workspaces/chess-club-host/shared/dns.tf
@@ -3,3 +3,11 @@ import {
   id = "Z0709817284VP6KJ4AIUQ"
 }
 
+resource "aws_route53_zone" "com_chessclubhost" {
+  comment           = "HostedZone created by Route53 Registrar"
+  delegation_set_id = null
+  force_destroy     = null
+  name              = "chessclubhost.com"
+  tags              = {}
+  tags_all          = {}
+}
---
+++ workspaces/stem-garden/shared/dns.tf
@@ -0,0 +1,21 @@
+import {
+  to = aws_route53_zone.org_stemgarden
+  id = "Z04644452DUB6MD4Z75A7"
+}
+
+resource "aws_acm_certificate" "org_stemgarden" {
+  certificate_authority_arn = null
+  certificate_body          = null
+  certificate_chain         = null
+  domain_name               = "stemgarden.org"
+  early_renewal_duration    = null
+  key_algorithm             = "RSA_2048"
+  private_key               = null # sensitive
+  subject_alternative_names = ["*.stemgarden.org", "stemgarden.org"]
+  tags                      = {}
+  tags_all                  = {}
+  validation_method         = "DNS"
+  options {
+    certificate_transparency_logging_preference = "ENABLED"
+  }
+}
