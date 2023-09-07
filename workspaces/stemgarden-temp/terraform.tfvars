e4920290de4a5b2d665de8281adec7a559df6422
Nigel Wilson <nigel@stemgarden.org>
2023-09-07 12:18:52 -0400

    Delete code for nonexistant workspace sg temp

--- workspaces/stemgarden-temp/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-domain_name = "stemgarden.org"
-
-common_tags = {
-  project = "stemgarden.org"
-  provisioned_by = "terraform_cloud"
-}
--- workspaces/stemgarden-temp/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "domain_name" {
-  type        = string
-  description = "The domain name for the website."
-}
-
-variable "common_tags" {
-  description = "Common tags you want applied to all components."
-}
