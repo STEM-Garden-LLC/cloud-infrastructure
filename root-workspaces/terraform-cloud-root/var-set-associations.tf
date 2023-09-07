0d180e31c54d35dc07b2994e9905ac1e742b9c41
Nigel Wilson <nigel@stemgarden.org>
2023-09-07 14:06:01 -0400

    Associate AWS keys var set with projects

--- root-workspaces/terraform-cloud-root/var-set-associations.tf
+++ root-workspaces/terraform-cloud-root/var-set-associations.tf
@@ -11,3 +11,32 @@ resource "tfe_project_variable_set" "gh_pat_to_sandbox" {
   variable_set_id = tfe_variable_set.gh_credentials.id
   project_id    = module.sandbox_project_and_workspaces.project_id
 }
+
+########################
+##   AWS Access Key   ##
+########################
+
+resource "tfe_workspace_variable_set" "aws_key_to_root" {
+  variable_set_id = tfe_variable_set.aws_credentials_nfw.id
+  workspace_id    = tfe_workspace.aws_root.id
+}
+
+resource "tfe_project_variable_set" "aws_key_to_sandbox" {
+  variable_set_id = tfe_variable_set.aws_credentials_nfw.id
+  project_id    = module.sandbox_project_and_workspaces.project_id
+}
+
+resource "tfe_project_variable_set" "aws_key_to_stemgarden" {
+  variable_set_id = tfe_variable_set.aws_credentials_nfw.id
+  project_id    = module.stem_garden_project_and_workspaces.project_id
+}
+
+resource "tfe_project_variable_set" "aws_key_to_masterymath" {
+  variable_set_id = tfe_variable_set.aws_credentials_nfw.id
+  project_id    = module.mastery_math_project_and_workspaces.project_id
+}
+
+resource "tfe_project_variable_set" "aws_key_to_masterymath" {
+  variable_set_id = tfe_variable_set.aws_credentials_nfw.id
+  project_id    = module.chess_club_host_project_and_workspaces.project_id
+}
