e293220eaca895a29a7e25af9fd43c683f0533a4
Nigel Wilson <nigel@stemgarden.org>
2023-09-07 13:30:20 -0400

    Add var set for AWS creds

--- root-workspaces/terraform-cloud-root/variable-sets.tf
+++ root-workspaces/terraform-cloud-root/variable-sets.tf
@@ -6,7 +6,7 @@
 
 resource "tfe_variable_set" "gh_credentials" {
   name          = "GitHub Credentials"
-  description   = "Some description."
+  description   = "Personal Access Token for nigel-f-wilson account on GitHub"
   organization  = tfe_organization.sgllc.name
 }
 
@@ -28,7 +28,7 @@ resource "tfe_variable" "gh_personal_access_token" {
 
 resource "tfe_variable_set" "gh_org_app_registration" {
   name          = "GitHub TFC App Installation"
-  description   = "The Terraform Cloud App that is installed in the STEM Garden LLC GitHub Org."
+  description   = "The Terraform Cloud App that is installed in the STEM Garden LLC GitHub Organization"
   organization  = tfe_organization.sgllc.name
 }
 
@@ -36,3 +36,35 @@ resource "tfe_variable_set" "gh_org_app_registration" {
 ###########
 ##  AWS  ##
 ###########
+
+resource "tfe_variable_set" "aws_credentials_nfw" {
+  name          = "AWS Credentials"
+  description   = "Access Key ID and Secret Access Key for AWS account with Admin access to SGLLC. User: nigel-f-wilson"
+  organization  = tfe_organization.sgllc.name
+}
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+resource "tfe_variable" "aws_access_key_id" {
+  description     = "nigel-f-wilson's AWS Access Key ID ending with \"HAYGS\""
+  key             = "AWS_ACCESS_KEY_ID"
+  value           = var.aws_access_key_id
+  sensitive       = true
+  category        = "env"
+  variable_set_id = tfe_variable_set.aws_credentials_nfw.id
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
+
+resource "tfe_variable" "aws_secret_access_key" {
+  description     = "nigel-f-wilson's AWS Secret Access Key. Goes with Key ID ending \"HAYGS\""
+  key             = "AWS_SECRET_ACCESS_KEY"
+  value           = var.aws_secret_access_key
+  sensitive       = true
+  category        = "env"
+  variable_set_id = tfe_variable_set.aws_credentials_nfw.id
+}
