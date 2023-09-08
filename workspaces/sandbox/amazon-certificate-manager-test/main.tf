952b7163e06cf71e6b248f3273d15050319390c7
Nigel Wilson <nigel@stemgarden.org>
2023-09-08 19:38:21 -0400

    Comment out test certificate in sandbox workspace

--- workspaces/sandbox/amazon-certificate-manager-test/main.tf
+++ workspaces/sandbox/amazon-certificate-manager-test/main.tf
@@ -1,6 +1,6 @@
-module "acm_certificate_test" {
-  source = "../../../modules/dns-certificate"
+# module "acm_certificate_test" {
+#   source = "../../../modules/dns-certificate"
 
-  apex_domain = "chessclubhost.com"
-  subject_alternative_names = [ "*.chessclubhost.com" ]
-}
+#   apex_domain = "chessclubhost.com"
+#   subject_alternative_names = [ "*.chessclubhost.com" ]
+# }
