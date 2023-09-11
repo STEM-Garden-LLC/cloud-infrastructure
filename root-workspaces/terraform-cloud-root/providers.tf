e53ae32e4eeb44cdfa8fb0a28cb3e520e9b8b2ef
Nigel Wilson <nigel@stemgarden.org>
2023-09-10 20:42:20 -0400

    Default tags not supported by TFC provider

--- root-workspaces/terraform-cloud-root/providers.tf
+++ root-workspaces/terraform-cloud-root/providers.tf
@@ -22,12 +22,12 @@ provider "tfe" {
   # token = var.token   # Set with env var TFE_TOKEN instead
   organization = "stem-garden-llc"
 
-  default_tags {
-    tags = {
-      Owner            = "SGLLC"
-      Project          = "Cloud Provider Roots"
-      Workspace        = "Terraform Cloud Root"
-      Managed_via      = "Terraform Cloud"
-    }
-  }
+  # default_tags {
+  #   tags = {
+  #     Owner            = "SGLLC"
+  #     Project          = "Cloud Provider Roots"
+  #     Workspace        = "Terraform Cloud Root"
+  #     Managed_via      = "Terraform Cloud"
+  #   }
+  # }
 }
