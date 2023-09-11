a51023d0cd69907e35191caab6c266b8b8efc15c
Nigel Wilson <nigel@stemgarden.org>
2023-09-11 00:00:53 -0400

    Add generated code to import nigel's IAM user

--- root-workspaces/aws-root/iam-users.tf
+++ root-workspaces/aws-root/iam-users.tf
@@ -2,3 +2,11 @@ import {
   to = aws_iam_user.nigel_wilson
   id = "nigel-f-wilson"
 }
+
+resource "aws_iam_user" "nigel_wilson" {
+  name                 = "nigel-f-wilson"
+  permissions_boundary = null
+  force_destroy        = null
+}
+
+# resource "aws_iam_user" "example" {
