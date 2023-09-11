7787556c6e1fe0c1f4f57cc9a3ea006fa0e16a04
Nigel Wilson <nigel@stemgarden.org>
2023-09-10 21:30:13 -0400

    Add generated config for Org and OU imports

--- root-workspaces/aws-root/aws-organization.tf
+++ root-workspaces/aws-root/aws-organization.tf
@@ -7,6 +7,12 @@ import {
   id = "o-c2jf6qw1ou"
 }
 
+resource "aws_organizations_organization" "sgllc_org" {
+  aws_service_access_principals = ["account.amazonaws.com"]
+  enabled_policy_types          = []
+  feature_set                   = "ALL"
+}
+
 # import {
 #   to = aws_organizations_organizational_unit.root_ou
 #   id = "r-p2mx"
@@ -17,6 +23,18 @@ import {
   id = "889823018333"
 }
 
+resource "aws_organizations_account" "stem_garden_llc" {
+  close_on_deletion          = null
+  create_govcloud            = null
+  email                      = "nigel@stemgarden.org"
+  iam_user_access_to_billing = null
+  name                       = "STEM Garden LLC"
+  parent_id                  = "r-p2mx"
+  role_name                  = null
+  tags                       = {}
+  tags_all                   = {}
+}
+
 ############################
 ##  STEM Garden Homepage  ##
 ############################
@@ -25,6 +43,16 @@ import {
   to = aws_organizations_organizational_unit.stem_garden_ou
   id = "ou-p2mx-9w3b2mnl"
 }
+
+resource "aws_organizations_organizational_unit" "stem_garden_ou" {
+  name      = "STEM Garden Homepage"
+  parent_id = "r-p2mx"
+  tags      = {}
+  tags_all  = {}
+}
+
+
+
 
 # No Accounts to import here, will create using TF
 # import {
@@ -41,15 +69,48 @@ import {
   id = "ou-p2mx-6hyxrmrf"
 }
 
+resource "aws_organizations_organizational_unit" "mastery_math_ou" {
+  name      = "Mastery Math"
+  parent_id = "r-p2mx"
+  tags      = {}
+  tags_all  = {}
+}
+
 import {
   to = aws_organizations_account.mastery_math_dev
   id = "496750456511"
+}
+
+
+resource "aws_organizations_account" "mastery_math_dev" {
+  close_on_deletion          = null
+  create_govcloud            = null
+  email                      = "masterymath+dev@stemgarden.org"
+  iam_user_access_to_billing = null
+  name                       = "Mastery Math Dev"
+  parent_id                  = "ou-p2mx-6hyxrmrf"
+  role_name                  = null
+  tags                       = {}
+  tags_all                   = {}
 }
 
 import {
   to = aws_organizations_account.mastery_math_prod
   id = "529386439094"
 }
+
+resource "aws_organizations_account" "mastery_math_prod" {
+  close_on_deletion          = null
+  create_govcloud            = null
+  email                      = "masterymath@stemgarden.org"
+  iam_user_access_to_billing = null
+  name                       = "Mastery Math Prod"
+  parent_id                  = "ou-p2mx-6hyxrmrf"
+  role_name                  = null
+  tags                       = {}
+  tags_all                   = {}
+}
+
 
 #########################
 ##  Club Hosting Tool  ##
