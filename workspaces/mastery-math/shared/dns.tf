e7fcedb8cdc25d16601a44f0736b1baa10eb1fd9
Nigel Wilson <nigel@stemgarden.org>
2023-09-10 20:38:19 -0400

    Remove MM DNS. Changes made to NS
    There was a mismatch in the NS records returned by whois and the route 53 console so I updated the records in route 53 but this may take 48 hours to propagate.

--- workspaces/mastery-math/shared/dns.tf
+++ workspaces/mastery-math/shared/dns.tf
@@ -1,20 +1,20 @@
 
-import {
-  to = aws_route53_zone.mastery_math
-  id = "Z06677023RZS93Q99ME56"
-}
+# import {
+#   to = aws_route53_zone.mastery_math
+#   id = "Z06677023RZS93Q99ME56"
+# }
 
-resource "aws_route53_zone" "mastery_math" {
-  comment           = "Managed by TFC"
-  delegation_set_id = null
-  force_destroy     = null
-  name              = "masterymath.org"
+# resource "aws_route53_zone" "mastery_math" {
+#   comment           = "Managed by TFC"
+#   delegation_set_id = null
+#   force_destroy     = null
+#   name              = "masterymath.org"
   
-}
+# }
 
-module "acm_certificate_for_masterymath" {
-  source = "../../../modules/dns-certificate"
+# module "acm_certificate_for_masterymath" {
+#   source = "../../../modules/dns-certificate"
 
-  apex_domain = "masterymath.org"
-  subject_alternative_names = [ "*.masterymath.org" ]
-}
+#   apex_domain = "masterymath.org"
+#   subject_alternative_names = [ "*.masterymath.org" ]
+# }
