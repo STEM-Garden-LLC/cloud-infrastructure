14bb5e53a7ba5f484ba2e626df72ac9f0e82b7c8
Nigel Wilson <nigel@stemgarden.org>
2023-09-10 20:44:52 -0400

    Don't delete Hosted Zone
    NS records were updated manually instead

--- workspaces/mastery-math/shared/dns.tf
+++ workspaces/mastery-math/shared/dns.tf
@@ -4,13 +4,13 @@
 #   id = "Z06677023RZS93Q99ME56"
 # }
 
-# resource "aws_route53_zone" "mastery_math" {
-#   comment           = "Managed by TFC"
-#   delegation_set_id = null
-#   force_destroy     = null
-#   name              = "masterymath.org"
+resource "aws_route53_zone" "mastery_math" {
+  comment           = "Managed by TFC"
+  delegation_set_id = null
+  force_destroy     = null
+  name              = "masterymath.org"
   
-# }
+}
 
 # module "acm_certificate_for_masterymath" {
 #   source = "../../../modules/dns-certificate"
