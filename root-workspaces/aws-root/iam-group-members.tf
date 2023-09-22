0b4b7cce441fc7588c7becd0c0741d60f804a4bc
Nigel Wilson <nigel@stemgarden.org>
2023-09-21 22:41:40 -0400

    Remove duplicate group declarations

--- root-workspaces/aws-root/iam-group-members.tf
+++ root-workspaces/aws-root/iam-group-members.tf
@@ -6,44 +6,15 @@
 #########################################
 
 resource "aws_iam_group_membership" "self_manage_login" {
-  name = "users-who-can-self-manage-login" # This will be ALL users so perhaps it makes more sense to create a group that is ALL users and attatch multiple policies including the self manage policy.  
+  name = "users-who-can-self-manage-login"  # This will be ALL users so perhaps it makes more sense to create a group that is ALL users and attatch multiple policies including the self manage policy.  
 
   users = [
-    aws_iam_user.user_one.name,
-    aws_iam_user.user_two.name,
+    aws_iam_user.nigel_wilson.name,
+    aws_iam_user.nigels-test-user.name,
+    aws_iam_user.bruce-lindman.name,
   ]
 
   group = aws_iam_group.group.name
-}
-
-resource "aws_iam_group" "self_manage_login" {
-  name = "self-manage-login"
-  path = "/"
-}
-
-resource "aws_iam_group_policy" "self_manage_login_policy" {
-  name  = "self_manage_login_policy"
-  group = aws_iam_group.self_manage_login.name
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_group" "sgllc_root_readonly" {
-  name = "sgllc-root-readonly"
-  path = "/"
 }
 
 
@@ -51,37 +22,14 @@ resource "aws_iam_group" "sgllc_root_readonly" {
 ##  STEM Garden Homepage  ##
 ############################
 
-resource "aws_iam_group" "stem_garden_admins" {
-  name = "stem-garden-admins"
-  path = "/"
-}
-
 
 ####################
 ##  Mastery Math  ##
 ####################
 
-resource "aws_iam_group" "mastery_math" {
-  name = "mastery-math"
-  path = "/"
-}
-
-resource "aws_iam_group" "mastery_math_admins" {
-  name = "mastery-math-admins"
-  path = "/mastery-math"
-}
-
-resource "aws_iam_group" "mastery_math_devs" {
-  name = "mastery-math-devs"
-  path = "/mastery-math"
-}
 
 
 #########################
 ##  Club Hosting Tool  ##
 #########################
 
-resource "aws_iam_group" "chessclubhost" {
-  name = "chess-club-host-admins"
-  path = "/"
-}
