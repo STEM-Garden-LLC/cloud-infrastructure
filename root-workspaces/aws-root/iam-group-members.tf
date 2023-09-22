3fb0adb71b0e5a04f36dffa85be709a39ef47ecf
Nigel Wilson <nigel@stemgarden.org>
2023-09-22 19:30:42 -0400

    Remove nigel-f-wilson from iam group all-team-members

--- root-workspaces/aws-root/iam-group-members.tf
+++ root-workspaces/aws-root/iam-group-members.tf
@@ -10,7 +10,7 @@ resource "aws_iam_group_membership" "all_team_members" {
   group = aws_iam_group.all_team_members.name
   # All users seen in iam-users.tf including imported and created
   users = setunion(
-    [ aws_iam_user.nigel_wilson.name ],
+    # [ aws_iam_user.nigel_wilson.name ],
     toset([ for profile in module.users : profile.username ])
   )
 }
