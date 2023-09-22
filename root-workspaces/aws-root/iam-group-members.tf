7df00f332c8b9f0d8c72b6c31277e3df68a82fb2
Nigel Wilson <nigel@stemgarden.org>
2023-09-21 22:50:23 -0400

    Fix refs to module created users and all_team group

--- root-workspaces/aws-root/iam-group-members.tf
+++ root-workspaces/aws-root/iam-group-members.tf
@@ -5,16 +5,16 @@
 ##    Read Root & Self Manage Login    ##
 #########################################
 
-resource "aws_iam_group_membership" "self_manage_login" {
+resource "aws_iam_group_membership" "all_team_members" {
   name = "users-who-can-self-manage-login"  # This will be ALL users so perhaps it makes more sense to create a group that is ALL users and attatch multiple policies including the self manage policy.  
 
   users = [
     aws_iam_user.nigel_wilson.name,
-    aws_iam_user.nigels-test-user.name,
-    aws_iam_user.bruce-lindman.name,
+    module.users.nigels-test-user.name,
+    module.users.bruce-lindman.name,
   ]
 
-  group = aws_iam_group.group.name
+  group = aws_iam_group.all_team_members
 }
 
 
