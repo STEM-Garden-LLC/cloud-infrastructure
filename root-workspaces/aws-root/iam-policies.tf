8a81304b4b48b778cae1b6fa566a171c8867bf3a
Nigel Wilson <nigel@stemgarden.org>
2023-09-25 18:49:33 -0400

    Delete basic self-manage password policy

--- root-workspaces/aws-root/iam-policies.tf
+++ root-workspaces/aws-root/iam-policies.tf
@@ -2,62 +2,6 @@
 ##  Organization & Management Account  ##
 #########################################
 
-resource "aws_iam_policy" "self_manage_password" {
-  name = "self_manage_password_and_api_keys_policy"
-  #docs: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage-pass-accesskeys-ssh.html
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "AllowViewAccountInfo",
-        "Effect" : "Allow",
-        "Action" : [
-          "iam:GetAccountPasswordPolicy",
-          "iam:GetAccountSummary",
-          "iam:ListUsers"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Sid" : "AllowManageOwnPasswords",
-        "Effect" : "Allow",
-        "Action" : [
-          "iam:ChangePassword",
-          "iam:GetUser",
-          "iam:GetLoginProfile",
-          "iam:UpdateLoginProfile"
-        ],
-        "Resource" : "arn:aws:iam::*:user/$${aws:username}"
-      },
-      {
-        "Sid" : "AllowManageOwnAccessKeys",
-        "Effect" : "Allow",
-        "Action" : [
-          "iam:CreateAccessKey",
-          "iam:DeleteAccessKey",
-          "iam:ListAccessKeys",
-          "iam:UpdateAccessKey",
-          "iam:GetAccessKeyLastUsed"
-        ],
-        "Resource" : "arn:aws:iam::*:user/$${aws:username}"
-      },
-      {
-        "Sid" : "AllowManageOwnSSHPublicKeys",
-        "Effect" : "Allow",
-        "Action" : [
-          "iam:DeleteSSHPublicKey",
-          "iam:GetSSHPublicKey",
-          "iam:ListSSHPublicKeys",
-          "iam:UpdateSSHPublicKey",
-          "iam:UploadSSHPublicKey"
-        ],
-        "Resource" : "arn:aws:iam::*:user/$${aws:username}"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_policy" "self_manage_mfa_and_credentials" {
   name = "self_manage_password_mfa_and_api_keys_policy"
   # docs: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage.html
