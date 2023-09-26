508290ca74c7c6e42122a5610c50fc4b7f62a61a
Nigel Wilson <nigel@stemgarden.org>
2023-09-26 17:24:41 -0400

    Update assume-role-group.tf

--- modules/assume_role_group/assume-role-group.tf
+++ modules/assume_role_group/assume-role-group.tf
@@ -1,6 +1,15 @@
-variable "group_name" {
+####################
+##   Input Vars   ##
+####################
+
+variable "project_name" {
   type = string
-  description = "An underscore separated string that indicates which Project(s) and Access Role types Group members can assume."
+  description = "An underscore separated string that indicates which Project the Group grants access to."
+}
+
+variable "access_type" {
+  type = string
+  description = "An underscore separated string that indicates the level of access granted by the Role ARNs below."
 }
 
 variable "group_members" {
@@ -13,3 +22,51 @@ variable "assumable_role_arns" {
   description = "Used to attach one or more Project Account Role to a single Group in the Management Account."
 }
 
+locals {
+  spaced_project_name = replace(var.project_name, "_", " ")
+  one_word_project_name = replace(var.project_name, "_", "")
+  group_name = "${var.project_name}_${var.access_type}"
+}
+
+
+#################
+##    Group    ##
+#################
+
+resource "aws_iam_group" "assume_role_group" {
+  name = local.group_name
+  path = "/${var.project_name}/"
+}
+
+###################
+##    Members    ##
+###################
+
+resource "aws_iam_group_membership" "assume_role_group" {
+  name = "${local.group_name}_members"  
+  group = aws_iam_group.assume_role_group.name
+  users = toset(var.group_members)
+}
+
+
+##########################
+##   Role Attachments   ##
+##########################
+
+resource "aws_iam_group_policy" "assume_role_group" {
+  for_each = toset(var.assumable_role_arns)
+
+  name = "${local.group_name}_policy"
+  group      = aws_iam_group.assume_role_group.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      { 
+        Sid = "AssumeRoleInMemberAccount"
+        Effect   = "Allow"
+        Action = "sts:AssumeRole"
+        Resource = "${each.value}"
+      },
+    ]
+  })
+}
