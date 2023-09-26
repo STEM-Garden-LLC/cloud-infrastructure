299b5d01dd17d0d0d9b3007b8c6d9e4eeb4ebd62
Nigel Wilson <nigel@stemgarden.org>
2023-09-26 14:32:14 -0400

    Complete converting assumable_roles to for_each

---
+++ modules/assumable_roles/assumable_roles.tf
@@ -0,0 +1,45 @@
+######################
+##    Input Vars    ##
+######################
+
+variable "trusted_account_id" {
+  type = string
+  description = "ID of the Management Account where IAM Users and Groups are created."
+  default = "889823018333"
+}
+
+variable "managed_access_policies" {
+  type = list(string)
+  description = "Path and Role names used to generate IAM managed policy assumable roles."
+  default = [ 
+    "AdministratorAccess",
+    "ReadOnlyAccess",
+    "job-function/Billing",
+    "job-function/DatabaseAdministrator",
+  ]
+}
+
+##########################
+##    Assumable Roles   ##
+##########################
+
+resource "aws_iam_role" "club_host_admin" {
+  for_each = toset(var.managed_access_policies)
+
+  name = "${each.value}"
+  description = "Role that Users in the Org's management account can assume to manage resources in member account."
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "EnableCrossAccountAssumeRole"
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        "Principal": { "AWS": "arn:aws:iam::${var.trusted_account_id}:root" }
+      },
+    ]
+  })
+  inline_policy {}
+  managed_policy_arns = "arn:aws:iam::aws:policy/${each.value}"
+}
+
