ceb7777eb682778d68fef179cf8c3459bc21e383
Nigel Wilson <nigel@stemgarden.org>
2023-09-26 23:30:33 -0400

    Fix var name

--- workspaces/sandbox/assumable-roles/main.tf
+++ workspaces/sandbox/assumable-roles/main.tf
@@ -7,7 +7,7 @@ module "sandbox_assumable_roles" {
   source = "../../../modules/assumable_roles"
 
   # These are the same as the default settings
-  trusted_account_id = var.trusted_account_id
+  trusted_account_id = var.management_account_id
   managed_access_policies = [ 
     "AdministratorAccess",
     "ReadOnlyAccess",
