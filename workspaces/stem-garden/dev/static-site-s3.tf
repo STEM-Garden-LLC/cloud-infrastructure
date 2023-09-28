0841361da14e8bcb430811cd0950616d1aa44380
Nigel Wilson <nigel@stemgarden.org>
2023-09-28 16:59:25 -0400

    Comment out static site module

--- workspaces/stem-garden/dev/static-site-s3.tf
+++ workspaces/stem-garden/dev/static-site-s3.tf
@@ -1,9 +1,9 @@
-module "static_site_s3" {
-  source = "../../../modules/s3-static-site"
+# module "static_site_s3" {
+#   source = "../../../modules/s3-static-site"
 
-  domain_name = "dev.stemgarden.org"
+#   domain_name = "dev.stemgarden.org"
 
-  use_preexisting_bucket = false
+#   use_preexisting_bucket = false
   
   
-}
+# }
