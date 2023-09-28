5ae3e317fd045ea904e4ddb3d1605a049ea98ad2
Nigel Wilson <nigel@stemgarden.org>
2023-09-28 17:15:48 -0400

    Update static-site-s3.tf

--- workspaces/stem-garden/prod/static-site-s3.tf
+++ workspaces/stem-garden/prod/static-site-s3.tf
@@ -1,9 +1,9 @@
-module "static_site_s3" {
-  source = "../../../modules/s3-static-site"
+# module "static_site_s3" {
+#   source = "../../../modules/s3-static-site"
 
-  domain_name = "stemgarden.org"
+#   domain_name = "stemgarden.org"
 
-  use_preexeisitng_bucket = false
+#   use_preexeisitng_bucket = false
 
   
-}
+# }
