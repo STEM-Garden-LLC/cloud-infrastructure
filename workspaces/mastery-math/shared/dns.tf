0d85e290e40af26b52434b222d02c5f531628b88
Nigel Wilson <nigel@stemgarden.org>
2023-09-27 23:20:43 -0400

    Comment out zone since provider uses new account role

--- workspaces/mastery-math/shared/dns.tf
+++ workspaces/mastery-math/shared/dns.tf
@@ -1,16 +1,16 @@
 
-# import {
-#   to = aws_route53_zone.mastery_math
-#   id = "Z06677023RZS93Q99ME56"
+
+# The Hosted Zone currently exists in the management account and a transfer is required since I updated the provider assume role.
+
+# resource "aws_route53_zone" "mastery_math" {
+#   comment           = "Managed by TFC"
+#   delegation_set_id = null
+#   force_destroy     = null
+#   name              = "masterymath.org"
 # }
 
-resource "aws_route53_zone" "mastery_math" {
-  comment           = "Managed by TFC"
-  delegation_set_id = null
-  force_destroy     = null
-  name              = "masterymath.org"
-  
-}
+
+
 
 # module "acm_certificate_for_masterymath" {
 #   source = "../../../modules/dns-certificate"
