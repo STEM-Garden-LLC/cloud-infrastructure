632304052a99891cfb9ff273f958622095ab4430
Nigel Wilson <nigel@stemgarden.org>
2023-09-28 12:38:38 -0400

    Delete iam-outputs.tf

--- root-workspaces/aws-root/iam-outputs.tf
+++
@@ -1,3 +0,0 @@
-# These are currently spread through the other iam-*.tf files. 
-# This blank file is just to remind me of an idea I had for a different 
-# way to organize should the curent system become unweildy.
