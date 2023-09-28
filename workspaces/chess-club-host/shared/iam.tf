d66a322c56a82573bf57f6976bd7d8377739ece2
Nigel Wilson <nigel@stemgarden.org>
2023-09-27 23:31:58 -0400

    Fix module source path

--- workspaces/chess-club-host/shared/iam.tf
+++ workspaces/chess-club-host/shared/iam.tf
@@ -5,5 +5,5 @@
 # Here in the sub-account we only need to create a Role that allows members of a Group in the Root Account to assume it. 
 
 module "club_host_assumable_roles" {
-  source = "../../../assumable_roles"
+  source = "../../../modules/assumable_roles"
 }
