7cd87b82cf2fbb0e4648df5664e33b3115c938fc
Nigel Wilson <nigel@stemgarden.org>
2023-09-30 16:39:57 -0400

    Create GH action secrets in SG repo

---
+++ workspaces/stem-garden/dev/github.tf
@@ -0,0 +1,21 @@
+data "github_repository" "stemgarden_dot_org" {
+  full_name = "STEM-Garden-LLC/stemgarden.org"
+}
+
+resource "github_actions_secret" "aws_s3_bucket" {
+  repository      = github_repository.stemgarden_dot_org.name
+  secret_name     = "AWS_S3_BUCKET"
+  plaintext_value  = module.static_site_s3.bucket_name
+}
+
+resource "github_actions_secret" "aws_access_key_id" {
+  repository      = github_repository.stemgarden_dot_org.name
+  secret_name     = "AWS_ACCESS_KEY_ID"
+  plaintext_value = var.aws_access_key_id
+}
+
+resource "github_actions_secret" "aws_secret_access_key" {
+  repository      = github_repository.stemgarden_dot_org.name
+  secret_name     = "AWS_SECRET_ACCESS_KEY"
+  plaintext_value = var.aws_secret_access_key
+}
