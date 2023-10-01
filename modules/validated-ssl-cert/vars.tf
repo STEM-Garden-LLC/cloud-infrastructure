53790e30c52a19d5e15de85da201fd9cbe834d1b
Nigel Wilson <nigel@stemgarden.org>
2023-10-01 00:11:32 -0400

    Provide required var, remove unused ones

--- modules/validated-ssl-cert/vars.tf
+++ modules/validated-ssl-cert/vars.tf
@@ -1,13 +1,3 @@
-variable "tfc_project" {
-  type        = string
-  description = "The name of the terraform cloud project this module is to be applied within."
-}
-
-variable "tfc_workspace" {
-  type        = string
-  description = "The name of the terraform cloud workspace this module is to be applied within."
-}
-
 variable "apex_domain" {
   type        = string
   description = "The apex domain used to retreive the Hosted Zone data."
--- workspaces/stem-garden/dev/dns.tf
+++ workspaces/stem-garden/dev/dns.tf
@@ -5,10 +5,7 @@ resource "aws_route53_zone" "main" {
 module "ssl_certificate" {
   source = "../../../modules/validated-ssl-cert"
 
-  tfc_project = var.tfc_project
-  tfc_workspace = var.tfc_workspace
-
+  hosted_zone_id = aws_route53_zone.main
   apex_domain = var.apex_domain
   sub_domain = var.sub_domain
-
 }
