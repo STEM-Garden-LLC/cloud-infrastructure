d554613803c0374f5bf977cd5dcdf3decebba985
Nigel Wilson <nigel@stemgarden.org>
2023-10-01 00:15:41 -0400

    Fix missing property hosted zone's zone_id

--- workspaces/stem-garden/dev/dns.tf
+++ workspaces/stem-garden/dev/dns.tf
@@ -5,7 +5,7 @@ resource "aws_route53_zone" "main" {
 module "ssl_certificate" {
   source = "../../../modules/validated-ssl-cert"
 
-  hosted_zone_id = aws_route53_zone.main
+  hosted_zone_id = aws_route53_zone.main.zone_id
   apex_domain = var.apex_domain
   sub_domain = var.sub_domain
 }
