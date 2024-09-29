9a46c2aec4e4f1e324de014cdc4a24feddd10a75
Nigel Wilson <nigel@stemgarden.org>
2024-09-28 22:56:08 -0500

    Try to resolve error by removing GH membership
    Commenting out to test solution to ERROR: 403 You must be a member of STEM-Garden-LLC to see membership information for nigel-f-wilson.

--- root-workspaces/github-root/gh-org-members.tf
+++ root-workspaces/github-root/gh-org-members.tf
@@ -2,10 +2,12 @@ data "github_user" "nigel-f-wilson" {
   username = "nigel-f-wilson"
 }
 
-resource "github_membership" "nigel_wilson" {
-  username = "nigel-f-wilson"
-  role     = "admin"
-}
+# Commenting out to test solution to ERROR: 403 You must be a member of STEM-Garden-LLC to see membership information for nigel-f-wilson.
+
+# resource "github_membership" "nigel_wilson" {
+#   username = "nigel-f-wilson"
+#   role     = "admin"
+# }
 
 # resource "github_membership" "bruce_lindman" {
 #   username = "sqlblindman"
